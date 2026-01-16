71d75f6c8cb9d373909803d9f46e1584973ef141
lastOfNoldor <martynovoo95@gmail.com>
2026-01-16 10:31:05 +0300

    final commit

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/hub/HubEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/model/hub/HubEvent.java
@@ -14,7 +14,10 @@ import java.time.Instant;
 @Setter
 @ToString
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "type", defaultImpl = ErrorEventType.class)
-@JsonSubTypes({@JsonSubTypes.Type(value = DeviceAddedEvent.class, name = "DEVICE_ADDED"), @JsonSubTypes.Type(value = DeviceRemovedEvent.class, name = "DEVICE_REMOVED"), @JsonSubTypes.Type(value = ScenarioAddedEvent.class, name = "SCENARIO_ADDED"), @JsonSubTypes.Type(value = ScenarioRemovedEvent.class, name = "SCENARIO_REMOVED")})
+@JsonSubTypes({@JsonSubTypes.Type(value = DeviceAddedEvent.class, name = "DEVICE_ADDED"),
+        @JsonSubTypes.Type(value = DeviceRemovedEvent.class, name = "DEVICE_REMOVED"),
+        @JsonSubTypes.Type(value = ScenarioAddedEvent.class, name = "SCENARIO_ADDED"),
+        @JsonSubTypes.Type(value = ScenarioRemovedEvent.class, name = "SCENARIO_REMOVED")})
 public abstract class HubEvent {
     @NotNull
     private String hubId;
