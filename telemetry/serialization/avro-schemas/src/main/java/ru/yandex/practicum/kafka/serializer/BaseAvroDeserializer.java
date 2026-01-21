aeaedc0dd080ed88b0cb2fdddd11a8e7226e0a94
lastOfNoldor <martynovoo95@gmail.com>
2026-01-21 11:11:39 +0300

    aggregator 2

---
+++ telemetry/serialization/avro-schemas/src/main/java/ru/yandex/practicum/kafka/serializer/BaseAvroDeserializer.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.kafka.serializer;
+
+import org.apache.avro.Schema;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.common.serialization.Deserializer;
+
+public class BaseAvroDeserializer<T extends SpecificRecordBase> implements Deserializer<T> {
+    // ....
+
+    public BaseAvroDeserializer(Schema schema) {
+        this(DecoderFactory.get(), schema);
+    }
+
+    public BaseAvroDeserializer(DecoderFactory decoderFactory, Schema schema) {
+        // ...
+    }
+
+    // ...
+
+    @Override
+    public T deserialize(String topic, byte[] data) {
+        // Код десериализации двоичных данных
+    }
+}
