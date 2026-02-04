830e79f4f40bda63794b6ff6d9c938d5329d24c4
lastOfNoldor <martynovoo95@gmail.com>
2026-02-05 01:16:46 +0300

    feat: analyzer added SnapshotService

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/service/SnapshotProcessor.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/service/SnapshotProcessor.java
@@ -5,8 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.analyzer.config.KafkaProperties;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 
 import java.time.Duration;
@@ -21,22 +23,47 @@ public class SnapshotProcessor {
     private final Consumer<String, SensorsSnapshotAvro> snapshotsConsumer;
     private final KafkaProperties props;
     private final String TOPIC = props.getSnapshots().getTopic();
-    private final SnapshotProcessor snapshotProcessor;
+    private final SnapshotService snapshotService;
 
     public void start() {
         log.info("Запуск SnapshotProcessor...");
-        snapshotsConsumer.subscribe(List.of(TOPIC));
+        try {
+            snapshotsConsumer.subscribe(List.of(TOPIC));
 
-        while (true) {
-            ConsumerRecords<String, SensorsSnapshotAvro> records = snapshotsConsumer.poll(Duration.ofMillis(100));
-            log.debug("Получено {} событий", records.count());
-            for (ConsumerRecord<String, SensorsSnapshotAvro> record : records) {
+            while (true) {
+                try {
+                    ConsumerRecords<String, SensorsSnapshotAvro> records = snapshotsConsumer.poll(Duration.ofMillis(100));
+                    if (!records.isEmpty()) {
+                        log.debug("Получено {} событий", records.count());
+                        processSnapshots(records);
+                    }
+                    snapshotsConsumer.commitSync();
+                } catch (Exception e) {
+                    log.error("Ошибка при обработке сообщений Snapshot Kafka", e);
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Получен сигнал WakeupException, начинаем graceful shutdown");
+        } finally {
+            log.info("Завершение HubEventProcessor...");
+            snapshotsConsumer.close();
+        }
+    }
+
+    private void processSnapshots(ConsumerRecords<String, SensorsSnapshotAvro> records) {
+        for (ConsumerRecord<String, SensorsSnapshotAvro> record : records) {
+            try {
                 log.debug("Обработка события: ключ={}, partition={}, offset={}",
                         record.key(), record.partition(), record.offset());
-                Optional<SensorsSnapshotAvro> snapshot = snapshotProcessor.updateState(record.value());
+                snapshotService.processSnapshot(record.value());
+            } catch (Exception e) {
+                log.error("Ошибка обработки события (partition={}, offset={}): {}",
+                        record.partition(), record.offset(), e.getMessage(), e);
             }
-            snapshotsConsumer.commitSync();
         }
+
+        snapshotsConsumer.commitSync();
+        log.debug("Зафиксированы offset для {} сообщений", records.count());
     }
 
     public void shutdown() {
